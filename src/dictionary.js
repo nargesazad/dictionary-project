5b6b77d633c4771a46a1b7480016f7f0ceae88c6
Narges Safari <95274071+nargesazad@users.noreply.github.com>
2022-07-09 11:25:02 +0430

    add api

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
 
 
 export default function Dictionary(){
-    function handelSearch(){
+    let [word,setWord]=useState("");
+    function handelRespons(res){
+    let data = res.data[0];
+    console.log(data);
+    }
 
+    function handelSearch(event){
+        event.preventDefault();
+        let url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        axios.get(url).then(handelRespons);
+
+    }
+
+    function handelWordChange(event){
+        setWord(event.target.value);
     }
     return(
         <div className="dictionary">
             <form className="dForm" onSubmit={handelSearch}>
-                <input type= "search" placeholder="Type your word" autoFocus={true} />
-                <button className="btn btn-info" >Search</button>
+                <input type= "search" placeholder="Type your word" onChange={handelWordChange}/>
+               
             </form>
         </div>
     );
 }
+
+
+ //<button type="submit" className="btn btn-info" >Search</button>
