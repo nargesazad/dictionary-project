f1a5d1eecba00391d81beb6bdc0b10f626d4b39e
Narges Safari <95274071+nargesazad@users.noreply.github.com>
2022-07-24 12:57:56 +0430

    fix warning in photos.js

--- src/photos.js
+++ src/photos.js
@@ -4,16 +4,16 @@ import "./result.css"
 
 export default function Photos(props){
     let [photos,setPhotos] =useState(null);
-let apiKey="563492ad6f917000010000015f041cd965ba4125be4cd3e9b0c5faa0";
-let apiUrl=`https://api.pexels.com/v1/search?query=${props.word}&per_page=3`;
-let headers ={ Authorization: `Bearer ${apiKey}`};
+const apiKey="563492ad6f917000010000015f041cd965ba4125be4cd3e9b0c5faa0";
+const apiUrl=`https://api.pexels.com/v1/search?query=${props.word}&per_page=3`;
+const headers ={ Authorization: `Bearer ${apiKey}`};
 
 function HandelPhotos(response){
     setPhotos(response.data.photos);
 }
 
 useEffect(()=>{axios.get(apiUrl,{headers: headers })
-.then(HandelPhotos);},[props.word]) 
+.then(HandelPhotos);},[props.word , apiUrl]) 
 if(photos){
     return(
         <div className="photos row">
